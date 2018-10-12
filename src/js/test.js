8128360907a2d99f722f8a219ca05e8cc31d0067
bao76296 <762963131@qq.com>
2018-10-12 09:28:15 +0800

    '70%'

--- src/js/test.js
+++ src/js/test.js
@@ -7,7 +7,7 @@ const index_main_render = require('./controller/index_main_controller');
 const find_render = require('./controller/find_controller');
 const order_render = require('./controller/order_controller');
 const mine_render = require('./controller/mine_controller');
-
+ 
 index_main_render.render();
 // find_render.render();
 // order_render.render();
