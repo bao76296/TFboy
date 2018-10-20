82255669e16f585ca16e4d21650d13c3e0d70f1a
bao76296 <762963131@qq.com>
2018-10-20 18:44:01 +0800

    'bater-scroll-chage'

--- src/js/controller/main_controller.js
+++ src/js/controller/main_controller.js
@@ -2,11 +2,18 @@ const main = require('../views/main.html');
 const _swiper_one = require('../views/main_swiper_one.html');
 const _banner = require('../views/main_banner.html');
 const _swiper_two = require('../views/main_swiper_two.html');
+const _main_list = require('../views/main_shoplist.html');
+const _main_shop_list = require('../views/main_shoplist_data.html')
+
 
 import main_swiper from '../models/main_data';
+import { async } from 'rxjs/internal/scheduler/async';
+
 
 var bannerData = [];
 var swpierTwoData = [];
+var shoplist = [];
+
 const render =  async () => {
         var main_html = Handlebars.compile(main);
         $('main').html(main_html());
@@ -21,7 +28,43 @@ const init_html = async () => {
     await swiper_html();
     await banner_html();
     await swiper_two_html();
+    await shop_list();
     
+}
+
+//商家列表
+const shop_list = async () => {
+    
+    var template = Handlebars.compile(_main_list);
+    var _html = template();
+    $('.__shoplist').html(_html);
+
+    template = Handlebars.compile(_main_shop_list);
+    _html = template({data : shoplist});
+    $('.shoplist').html(_html);
+    await getShopList();
+
+
+//    console.log(shoplist,123)
+}
+
+//添加 商家列表
+const getShopList = async () => {
+    await getShoplistData();
+    var template = Handlebars.compile(_main_shop_list);
+    var _html = template({data : shoplist});
+    $('.shoplist').html(_html);
+}
+
+
+
+const getShoplistData = async () => {
+    
+    var temps = (await main_swiper.main_shoplist()).items;
+    setImageHash(temps, 'image_path')
+    shoplist = temps;
+    // shoplist.push( ... ((await main_swiper.main_shoplist()).items));
+    console.log(shoplist)
 }
 
 //打折商品
@@ -30,7 +73,6 @@ const banner_html = async () => {
     var template = Handlebars.compile(_banner);
     var _html = template({banner_data : bannerData[1]});
     $('.__banner').html(_html);
-    
 }
 
 //第一个轮播图的数据处理
@@ -38,9 +80,9 @@ const getSwiperData = async  () => {
     bannerData =  await main_swiper.main_swiper();
     bannerData[0].entriesSlice = [];
     var arr =  bannerData[0].entries;
-    setImageHash(arr);
+    setImageHash(arr); 
     arr_slice(bannerData[0].entriesSlice, arr, 10);
-}
+} 
 
 //第一个轮播图的渲染
 const swiper_html = async () => {
@@ -77,13 +119,20 @@ const swiper_two_html = async() => {
         pagination: {
             el: '.swiper-pagination',
             type: 'bullets',
-          }
+        }
     })
 }
 
-const setImageHash = arr => {
+const setImageHash = (arr, prop) => {
+    console.log(arr)
     arr.forEach(item => {
-        var str = item.image_hash; 
+        let str = '';
+        if(prop){
+            str = item.restaurant.image_path ;
+        }
+        else{
+            str = item.image_hash;
+        }
         item.image_hash = str.substring(0,1) + '/'+ str.substring(1,3) + '/' + str.substring(3,str.length);
         if(item.more){
             item.more = JSON.parse(item.more);
--- src/js/models/main_data.js
+++ src/js/models/main_data.js
@@ -1,5 +1,5 @@
 
-
+//第一个轮播图  和  banner 数据
 const main_swiper = () => {
     return $.ajax({
         url : '/api/restapi/shopping/openapi/entries?latitude=47.352424&longitude=123.931074&templates[]=main_template&templates[]=favourable_template&templates[]=svip_template&terminal=h5',
@@ -9,6 +9,8 @@ const main_swiper = () => {
         }
     })
 }
+
+//第二个轮播图
 const main_two_swiper = () => {
     return $.ajax({
         url : '/api/restapi/shopping/v2/banners?consumer=1&type=1&latitude=47.352424&longitude=123.931074',
@@ -18,7 +20,20 @@ const main_two_swiper = () => {
         }
     })
 }
+
+//商家列表  https://h5.ele.me/restapi/shopping/v3/restaurants?latitude=47.352424&longitude=123.931074&offset=8&limit=8&extras[]=activities&extras[]=tags&extra_filters=home&rank_id=7f96ecea64244bbb8150e686d2f2cb47&terminal=h5
+const main_shoplist = () => {
+    return $.ajax({
+        url : '/api/restapi/shopping/v3/restaurants?latitude=47.352424&longitude=123.931074&offset=8&limit=8&extras[]=activities&extras[]=tags&extra_filters=home&rank_id=7f96ecea64244bbb8150e686d2f2cb47&terminal=h5',
+        type : 'get',
+        success : res => {
+            return res;
+        }
+    })
+}
+
 export default {
     main_swiper,
-    main_two_swiper
+    main_two_swiper,
+    main_shoplist
 };
