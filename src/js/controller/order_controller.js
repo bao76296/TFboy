de2f189dceac70e3d377ebbb6bf119ffa0f33505
zaoqidechongzibeiniaochi <97510234@qq.com>
2018-10-23 08:13:36 +0800

    陈爸爸的最后一次提交

--- src/js/controller/order_controller.js
+++ src/js/controller/order_controller.js
@@ -1,22 +1,42 @@
 const order_html = require('../views/order.html');
 const mock = require('../../static/mock/mock')
 const render = () => {
-    $.ajax({
-        url : 'http://localhost:8080/static/mock/order.json',
-        // url:'/list',
-        // dataType : 'jsonp',
-        success:(data)=>{
-            var template = Handlebars.compile(order_html);
-            var template_str = template({data})
-            $('main').html(template_str);
-            // res.forEach(ele => {
-            //     console.log(ele)
+    // $.ajax({
+    //     url : 'http://localhost:8080/static/mock/order.json',
+    //     // url:'/list',
+    //     // dataType : 'jsonp',
+    //     success:(data)=>{
+    //         var template = Handlebars.compile(order_html);
+    //         var template_str = template({data})
+    //         $('main').html(template_str);
+    //         // res.forEach(ele => {
+    //         //     console.log(ele)
                 
-            // });
+    //         // });
+    //     }
+
+    // })
+    var loca = window.localStorage;
+    var data = JSON.parse(loca.shopcar);
+    var time = new Date().toLocaleString();
+    data.time = time;
+    var template = Handlebars.compile(order_html);
+    var template_str = template({data})
+    
+    $('main').html(template_str);
+    var timespan = $('.timespan')
+    var ti = 10;
+
+    var timer = setInterval(()=>{
+        timespan.html(ti--)
+        if(ti<=0){
+            clearInterval(timer)
+            var currentstatusoforder = $('.current-status-of-order')
+            var order_paybtn = $('.order-paybtn')
+            currentstatusoforder.html('订单超时已取消')
+            order_paybtn.css({display:'none'})
         }
-
-    })
-
+    },1000)
 }
 
 
